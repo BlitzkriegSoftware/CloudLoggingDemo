1f19ad3fe86ac150324b7897302de41397c3cd44
Stuart Williams <spookdejur@hotmail.com>
2020-05-09 15:33:34 -0700

    Much Better Demo, don't look at the old one!

--- GenerateLogs/Service/LogMakerService.cs
+++ GenerateLogs/Service/LogMakerService.cs
@@ -7,7 +7,6 @@ namespace GenerateLogs.Service
 {
     public class LogMakerService : ILogMakerService
     {
-        private readonly ILogger _logger;
         private readonly ILoggerFactory _loggerFactory;
         private readonly IConfigurationRoot _config;
         private readonly BlitzkriegSoftware.SecureRandomLibrary.SecureRandom Dice = new BlitzkriegSoftware.SecureRandomLibrary.SecureRandom();
@@ -17,13 +16,12 @@ namespace GenerateLogs.Service
         public LogMakerService(ILoggerFactory loggerFactory, IConfigurationRoot config)
         {
             this._loggerFactory = loggerFactory;
-            this._logger = loggerFactory.CreateLogger<LogMakerService>();
             this._config = config;
         }
 
         public void Run()
         {
-            this.DumpConfig(this._loggerFactory.CreateLogger("ConfigDumper"));
+            this.DumpConfig();
 
             for (int i=0; i< this.Loops; i++)
             {
@@ -32,9 +30,10 @@ namespace GenerateLogs.Service
             }
         }
 
-        private void DumpConfig(ILogger logger)
+        private void DumpConfig()
         {
-            foreach(var c in this._config.AsEnumerable())
+            ILogger logger = this._loggerFactory.CreateLogger("DumpConfig");
+            foreach (var c in this._config.AsEnumerable())
             {
                 logger.LogDebug("{0}: {1}", c.Key, c.Value);
             }
@@ -42,6 +41,8 @@ namespace GenerateLogs.Service
 
         private void MakeLogs()
         {
+            var logger = this._loggerFactory.CreateLogger("MakeLogs");
+
             var index = this.Dice.Next(1, 100);
             Exception ex0;
 
@@ -49,27 +50,27 @@ namespace GenerateLogs.Service
             {
                 case int n when (n < 10):
                      ex0 = new Models.RequiredConfigurationMissingException("ConfigKeyA", "Application will not run correctly!");
-                    this._logger.LogCritical(ex0, "{0}", ex0.ToString());
+                    logger.LogCritical(ex0, "{0}", ex0.ToString());
                     break;
                 case int n when (n >= 10 && n < 20):
                     ex0 = new Models.RequiredConfigurationBadValueException("ConfigKeyA", -20, "Application will not run correctly until this key has a valid value supplied (expected (int) 1-1000)");
-                    this._logger.LogCritical(ex0, "{0}", ex0.ToString());
+                    logger.LogCritical(ex0, "{0}", ex0.ToString());
                     break;
                 case int n when (n >= 20 && n < 40):
                     ex0 = new InvalidOperationException("The code did a bad thing, this is why... Process will not crash, but a unit of work may have been lost");
-                    this._logger.LogError(ex0, "{0}", ex0.ToString());
+                    logger.LogError(ex0, "{0}", ex0.ToString());
                     break;
                 case int n when (n >= 40 && n < 50):
                     ex0 = new TimeoutException("It took a couple of tries, but it worked eventually");
-                    this._logger.LogWarning(ex0, "{0}", ex0.ToString());
+                    logger.LogWarning(ex0, "{0}", ex0.ToString());
                     break;
                 case int n when (n >= 50 && n < 70):
-                    this._logger.LogInformation("This is information for **Business** Users, do not mis-use this for developer messages...");
+                    logger.LogInformation("This is information for **Business** Users, do not mis-use this for developer messages...");
                     break;
                 case int n when (n >= 70 && n < 80):
-                    this._logger.LogDebug("Debug (coarse debugging) for Developers");
+                    logger.LogDebug("Debug (coarse debugging) for Developers");
                     break;
-                default: this._logger.LogTrace("Trace (very detailed debugging) For Developers"); break;
+                default: logger.LogTrace("Trace (very detailed debugging) For Developers"); break;
             }
         }
 
